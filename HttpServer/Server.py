35b04038383bd368398cd45d3597a96258791abf
Dongliang Zhou <leoaction@gmail.com>
2018-06-08 13:58:12 -0400

    http server

---
+++ HttpServer/Server.py
@@ -0,0 +1,95 @@
+"""
+Very simple HTTP server in python.
+Usage::
+    ./dummy-web-server.py [<port>]
+Send a GET request::
+    curl http://localhost
+Send a HEAD request::
+    curl -I http://localhost
+Send a POST request::
+    curl -d "foo=bar&bin=baz" http://localhost
+"""
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from os import curdir, sep
+import SocketServer
+
+homepage = '''<HTML>
+<HEAD>
+<TITLE>Noosa Warehosue Home</TITLE>
+</HEAD>
+<BODY BGCOLOR="FFFFFF">
+<CENTER><IMG SRC="https://oukosher.org/content/uploads/2013/06/Noosa-1-1.jpg" ALIGN="BOTTOM"> </CENTER>
+<HR>
+<H1><a href="./shipping">Shipping Monitor</a></H1>
+<H1><a href="./receiving">Receiving Monitor</a></H1>
+<HR>
+</BODY>
+</HTML>'''
+
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+		if self.path=="/":
+			self.path="/homepage.html"
+
+		try:
+			#Check the file extension required and
+			#set the right mime type
+
+			sendReply = False
+			if self.path.endswith(".html"):
+				mimetype='text/html'
+				sendReply = True
+			if self.path.endswith(".jpg"):
+				mimetype='image/jpg'
+				sendReply = True
+			if self.path.endswith(".gif"):
+				mimetype='image/gif'
+				sendReply = True
+			if self.path.endswith(".js"):
+				mimetype='application/javascript'
+				sendReply = True
+			if self.path.endswith(".css"):
+				mimetype='text/css'
+				sendReply = True
+
+			if sendReply == True:
+				#Open the static file requested and send it
+				f = open(curdir + sep + self.path) 
+				self.send_response(200)
+				self.send_header('Content-type',mimetype)
+				self.end_headers()
+				self.wfile.write(f.read())
+				f.close()
+			return
+
+
+		except IOError:
+			self.send_error(404,'File Not Found: %s' % self.path)
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+        
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
