0c4fa9c74a9de57b1f55660fc9e5ef4ce8e61d02
haor <haor@andrew.cmu.edu>
2018-06-10 16:24:32 -0400

    ADD: radio and serial

--- RadioSend/Radio_obj_2/RadioXmit/RadioXmit.ino
+++ RadioSend/Radio_obj_2/RadioXmit/RadioXmit.ino
@@ -51,23 +51,44 @@ void setup()
       while(!radio.isChipConnected());
   }
   
-  radio.openWritingPipe(address);  // Open the radio pipe using your address (read about pipes and channels)
-  radio.setPALevel(RF24_PA_MIN);   // Set the power level. Since the bots and the radio base station are close I use min power
-  radio.stopListening();           // Now we listen for messages...
   
+//  radio.openReadingPipe(0, address);  // Open the radio pipe using your address (read about pipes and channels)
+//  radio.setPALevel(RF24_PA_MIN);      // Set the power level. Since the bots and the radio base station are close I use min power
+//  radio.startListening();             // Go into receive mode.
+//  Serial.println("Radio Read Ready...");
 }
 
 void loop()                        // We just send hello world broadcasts every second
 {
-  
   while(mySerial.available())
   {
+    //open write
+    radio.openWritingPipe(address);  // Open the radio pipe using your address (read about pipes and channels)
+    radio.setPALevel(RF24_PA_MIN);   // Set the power level. Since the bots and the radio base station are close I use min power
+    radio.stopListening();           // Now we listen for messages...
+  
     char content[16] = {};
-   
-    
     mySerial.readBytes(content, 16);   // Here is where we read the data
-    
     Serial.println(content);
     radio.write(&content, sizeof(content));
+
+     //open read
+    radio.openReadingPipe(0, address);  // Open the radio pipe using your address (read about pipes and channels)
+    radio.setPALevel(RF24_PA_MIN);      // Set the power level. Since the bots and the radio base station are close I use min power
+    radio.startListening();             // Go into receive mode.
+    Serial.println("Radio Read Ready...");
+
+  }
+
+  
+
+  while (radio.available())
+  {
+    char cmd[10] = "";
+    radio.read(&cmd, 3);
+    Serial.print("Receive command: ");
+    Serial.println(cmd);
+    mySerial.write(cmd);
+    //delay(1000);
   }
 }
---
+++ RadioSend/serialReadWrite.py
@@ -0,0 +1,62 @@
+###################################################################################################################
+# File: serialwriter.py
+# Course: 17640
+# Project: IoT Order Fulfillment Center
+# Copyright: Copyright (c) 2018 Carnegie Mellon University (ajl)
+# Versions:
+#	1.0 April 2018 - Initial write (ajl).
+#
+# Description: This class serves as an example for how to write an application that can write serial data to
+# an external device. The intent is for this to illustrate how to write data to an Arduino software serial
+# port. This example could be used as a basis for writing an application to send command to fulfillment
+# center robots.
+#
+# Parameters: Port or device file
+#
+# Internal Methods:
+#  None
+#
+# External Dependencies:
+#   - python 2.7
+#	- time
+#	- serial ## pyserial-3.2.1 library
+###################################################################################################################
+
+import sys
+import time
+import serial 
+import thread
+
+class SerialReadWrite(object):
+
+
+	def __init__(self, port, carState):
+		self.ser = serial.Serial(
+		    port=port,
+		    baudrate=9600,
+		    parity=serial.PARITY_NONE,
+		    stopbits=serial.STOPBITS_ONE,
+		    bytesize=serial.EIGHTBITS
+		)
+		self.carState = carState
+		time.sleep(1)
+
+	def carMove(self, carNo):
+		message = str(carNo) + "m"
+		b = str.encode(message)						# Puts the message into bytes
+		self.ser.write(b)								# Writes the bytes to the specified port 
+		self.ser.flush()
+
+	def listen(self):
+		while True: 
+			if self.ser.inWaiting() > 0:				# Check to see if anything is in the buffer					
+			line = self.ser.readline()			# Read the buffer
+
+			print('read::' + line +'\n')
+			time.sleep(1)
+			#
+
+	def run(self):
+		thread.start_new_thread(self.listen)
+								
+
