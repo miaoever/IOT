0ab47724724fc53911579d840bea39bb534e823f
zwen <zwen@andrew.cmu.edu>
2018-06-10 18:06:26 -0400

    zwen: modify return of getNextOrder

--- AdminApp/order/order.py
+++ AdminApp/order/order.py
@@ -9,18 +9,23 @@ class Order:
 
     def getNextOrder(self, order_id):
         query = Orders.select().where(Orders.id == order_id)
+        order_item = []
         try:
             order = query.where(Orders.id == order_id).get()
             order_ori = [order.black, order.blue, order.green, order.yellow, order.red, order.white]
-            order_item = []
             for i in order_ori:
                 if i is None:
                     order_item.append(0)
                 else:
                     order_item.append(i)
+
+            if order.fill is False:
+                order_status = 1  # order is not fulfilled
+            else:
+                order_status = 2   # order is fulfilled
         except Orders.DoesNotExist:
-            order_item = None
-        return order_item
+            order_status = 0  # no recorde
+        print {"order_status": order_status, "order_item": order_item}
 
     def markOrderFill(self, order_id):
         query = Orders.select().where(Orders.id == order_id)
@@ -31,3 +36,8 @@ class Order:
         else:
             return False
 
+
+o = Order()
+o.getNextOrder(8)
+o.getNextOrder(9)
+o.getNextOrder(10)
