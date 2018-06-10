d84294c32bdb2afd552dabcb86fffa1b80c08e90
zwen <zwen@andrew.cmu.edu>
2018-06-10 16:02:25 -0400

    zwen: getNextOrder return 0 instead of None

--- AdminApp/order.py
+++ AdminApp/order.py
@@ -11,7 +11,13 @@ class Order:
         query = Orders.select().where(Orders.id == order_id)
         if query.exists():
             order = Orders.get(Orders.id == order_id)
-            order_item = [order.id, order.black, order.blue, order.green, order.yellow, order.red, order.white]
+            order_ori = [order.black, order.blue, order.green, order.yellow, order.red, order.white]
+            order_item = []
+            for i in order_ori:
+                if i is None:
+                    order_item.append(0)
+                else:
+                    order_item.append(i)
             return order_item
         else:
             return None
