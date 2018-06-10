fdb6c3e28fe37f012696ccdd3e099803c8d7806e
zwen <zwen@andrew.cmu.edu>
2018-06-10 17:49:02 -0400

    zwen: markOrderFill update filldate

--- AdminApp/order/order.py
+++ AdminApp/order/order.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.7
 from models import Orders
-import json
+from time import gmtime, strftime
 
 
 class Order:
@@ -9,8 +9,8 @@ class Order:
 
     def getNextOrder(self, order_id):
         query = Orders.select().where(Orders.id == order_id)
-        if query.exists():
-            order = Orders.get(Orders.id == order_id)
+        try:
+            order = query.where(Orders.id == order_id).get()
             order_ori = [order.black, order.blue, order.green, order.yellow, order.red, order.white]
             order_item = []
             for i in order_ori:
@@ -18,17 +18,16 @@ class Order:
                     order_item.append(0)
                 else:
                     order_item.append(i)
-            return order_item
-        else:
-            return None
+        except Orders.DoesNotExist:
+            order_item = None
+        return order_item
 
     def markOrderFill(self, order_id):
         query = Orders.select().where(Orders.id == order_id)
         if query.exists():
-            Orders.update(fill=True).where(Orders.id == order_id).execute()
-            return True
+            current_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+            Orders.update(fill=True, filldate=current_time).where(Orders.id == order_id).execute()
+            return  True
         else:
             return False
 
-o = Order()
-o.getNextOrder(1)
