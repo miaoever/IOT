efca3466a1919566f4ef84c6d6bc51c6c8506558
jsclee <jsclee2013@gmail.com>
2018-08-03 20:10:05 -0400

    io through s3

--- Final/entry.py
+++ Final/entry.py
@@ -1,4 +1,7 @@
 import data_preprocess as dp
+import boto3 as boto3
+import os
+import os.path
 
 class mlEntry:
     bucket_name = 'iot-robotdata-noosa'
@@ -18,6 +21,14 @@ class mlEntry:
     def startUpdate(self):
         self.data_processor.start()
 
+    def upload_dir_s3(self, dir):
+        s3 = boto3.resource('s3')
+        for file_name in os.listdir(dir):
+            s3.meta.client.upload_file(dir+file_name, self.bucket_name, dir + file_name)
+
+
 if __name__ == "__main__":
     ml = mlEntry()
     ml.startUpdate()
+    ml.upload_dir_s3(ml.feature_path)
+    ml.upload_dir_s3(ml.plot_path)
